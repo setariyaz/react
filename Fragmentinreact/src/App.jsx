2ea41fcac880e92d6440f3f8b8766a7aeb0a235d
setariyaz <setariyaj@gmail.com>
2026-02-21 06:19:59 +0530

    Auto commit from batch file

---
+++ Fragmentinreact/src/App.jsx
@@ -0,0 +1,7 @@
+import Student from "./Student";
+
+function App()
+{
+  return <Student />
+}
+export default App;
---
+++ Fragmentinreact/src/Student.jsx
@@ -0,0 +1,10 @@
+function Student(){
+    return (
+        <>
+            <h1>hello Wlecome</h1>
+            <h1>my name is riyaz</h1>
+            
+        </>
+    )
+}
+export default Student;
